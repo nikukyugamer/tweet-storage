31f36bfd3e5e04f7f698d2965eba9a8c477e70d9
corselia <takiya@toran.sakura.ne.jp>
2020-05-05 15:50:30 +0900

    Add migration files for User and Tweet

--- db/migrate/20200505062934_create_users.rb
+++ db/migrate/20200505062934_create_users.rb
@@ -1,8 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.bigint 'id_number', null: false
+      # These two columns are verbose..., but with them, we human can read records smoothly
+      t.string 'name', null: false
+      t.string 'screen_name', null: false
+      t.json 'serialized_object', null: false
 
       t.timestamps
     end
+
+    add_index :users, :id_number
+    add_index :users, :name
+    add_index :users, :screen_name
   end
 end
