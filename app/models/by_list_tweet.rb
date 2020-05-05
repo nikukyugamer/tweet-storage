38ca53cbedb503b685200b73d6978a219758bb83
corselia <takiya@toran.sakura.ne.jp>
2020-05-05 16:57:36 +0900

    Add validates to STI models

--- app/models/by_list_tweet.rb
+++ app/models/by_list_tweet.rb
@@ -1,2 +1,5 @@
 class ByListTweet < Tweet
+  validates :list_id_number, presence: true
+  validates :list_name, presence: true
+  validates :list_serialized_object, presence: true
 end
--- app/models/by_search_word_tweet.rb
+++ app/models/by_search_word_tweet.rb
@@ -1,2 +1,3 @@
 class BySearchWordTweet < Tweet
+  validates :search_word, presence: true
 end
