59cb5a98f1046fcc269fec28f62c8e3e99b64789
corselia <takiya@toran.sakura.ne.jp>
2020-05-19 14:11:59 +0900

    Fix "if" logic that using not 'nil?' but 'present?'

--- app/lib/operations/gensosenkyo_2020/database.rb
+++ app/lib/operations/gensosenkyo_2020/database.rb
@@ -10,7 +10,7 @@ module Operations
         # 最も使われるメソッド
         def write_by_next_tweet_id_number_search(search_query)
           tweets = TwitterApi::CollectTweet.continue_from_next_tweet_id_number_with_loop_by_search(search_query)
-          ::Database::SaveTweet.by_search_word_tweet(search_query, tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_search_word_tweet(search_query, tweets) if tweets.present?
         end
 
         # APIでのツイート取得を伴う
@@ -18,7 +18,7 @@ module Operations
         # 初めてのツイート取得からのデータベース格納の際に用いるメソッド
         def initial_write_by_search(search_query)
           tweets = TwitterApi::CollectTweet.all_by_search(search_query)
-          ::Database::SaveTweet.by_search_word_tweet(search_query, tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_search_word_tweet(search_query, tweets) if tweets.present?
         end
 
         # APIでのツイート取得を伴う
@@ -28,7 +28,7 @@ module Operations
         # API の使用量を明示的に抑制できる
         def write_by_general_search(search_query)
           tweets = TwitterApi::CollectTweet.by_search(search_query)
-          ::Database::SaveTweet.by_search_word_tweet(search_query, tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_search_word_tweet(search_query, tweets) if tweets.present?
         end
 
         # 初期化用
@@ -36,7 +36,7 @@ module Operations
           target_list_from_api = TwitterApi::CollectList.specific_list(list_identify)
           tweets = TwitterApi::CollectTweet.all_by_specific_list(list_identify, options)
 
-          ::Database::SaveTweet.by_list_tweet(target_list_from_api, tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_list_tweet(target_list_from_api, tweets) if tweets.present?
         end
 
         # 2回目以降用
@@ -47,13 +47,13 @@ module Operations
           options = options.merge({ since_id: max_id_number_tweet_in_db })
 
           tweets = TwitterApi::CollectTweet.all_by_specific_list(list_identify, options)
-          ::Database::SaveTweet.by_list_tweet(target_list_from_api, tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_list_tweet(target_list_from_api, tweets) if tweets.present?
         end
 
         # 初期化用
         def write_by_specific_user_tweet(user_identify, options = { tweet_mode: 'extended', count: 200, since_id: 1, max_id: 9_000_000_000_000_000_000 })
           tweets = TwitterApi::CollectTweet.all_by_specific_user(user_identify, options)
-          ::Database::SaveTweet.by_specific_user_tweet(tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_specific_user_tweet(tweets) if tweets.present?
         end
 
         # 2回目以降用
@@ -66,7 +66,7 @@ module Operations
           options = options.merge({ since_id: max_id_number_tweet_in_db })
 
           tweets = TwitterApi::CollectTweet.all_by_specific_user(user_identify, options)
-          ::Database::SaveTweet.by_specific_user_tweet(tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_specific_user_tweet(tweets) if tweets.present?
         end
 
         # 特定のユーザ（単数）のレコードを追加する
@@ -87,7 +87,7 @@ module Operations
         # 取得に失敗したツイートに対しては空が返る（例外は返らない）
         def write_tweets_by_specific_tweet_id_numbers(*id_numbers)
           tweets = ::TwitterApi::CollectTweet.specific_tweets_by_tweet_id_numbers(id_numbers)
-          ::Database::SaveTweet.by_specific_id_tweets(tweets) unless tweets.nil?
+          ::Database::SaveTweet.by_specific_id_tweets(tweets) if tweets.present?
         end
 
         # 特定のリストのレコードを追加する
