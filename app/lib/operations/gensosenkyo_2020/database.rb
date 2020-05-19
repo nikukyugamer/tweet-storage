0a79c1f7b636f415f7bad440d1e0c6f973d38240
corselia <takiya@toran.sakura.ne.jp>
2020-05-19 12:05:05 +0900

    Add 'extended' option

--- app/lib/operations/gensosenkyo_2020/database.rb
+++ app/lib/operations/gensosenkyo_2020/database.rb
@@ -32,7 +32,7 @@ module Operations
         end
 
         # 初期化用
-        def write_by_list(list_identify, options = { count: 200, since_id: 1, max_id: 9_000_000_000_000_000_000 })
+        def write_by_list(list_identify, options = { tweet_mode: 'extended', count: 200, since_id: 1, max_id: 9_000_000_000_000_000_000 })
           target_list_from_api = TwitterApi::CollectList.specific_list(list_identify)
           tweets = TwitterApi::CollectTweet.all_by_specific_list(list_identify, options)
 
@@ -40,7 +40,7 @@ module Operations
         end
 
         # 2回目以降用
-        def write_next_tweet_by_list(list_identify, options = { count: 200, since_id: 1, max_id: 9_000_000_000_000_000_000 })
+        def write_next_tweet_by_list(list_identify, options = { tweet_mode: 'extended', count: 200, since_id: 1, max_id: 9_000_000_000_000_000_000 })
           # データベースに記録する情報は最新の情報にしたいので、API から取得した情報を用いる
           target_list_from_api = TwitterApi::CollectList.specific_list(list_identify)
           max_id_number_tweet_in_db = Tweet.by_specific_list_with_id_number(target_list_from_api.id).max_id_number
@@ -51,7 +51,7 @@ module Operations
         end
 
         # 初期化用
-        def write_by_specific_user_tweet(user_identify, options = { count: 200, since_id: 1, max_id: 9_000_000_000_000_000_000 })
+        def write_by_specific_user_tweet(user_identify, options = { tweet_mode: 'extended', count: 200, since_id: 1, max_id: 9_000_000_000_000_000_000 })
           tweets = TwitterApi::CollectTweet.all_by_specific_user(user_identify, options)
           ::Database::SaveTweet.by_specific_user_tweet(tweets)
         end
