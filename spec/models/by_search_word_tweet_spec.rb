424d1b4bb3e7bb57ce357ef9f736ee24b79f6a53
nikukyugamer <takiya@toran.sakura.ne.jp>
2020-05-28 06:23:35 +0900

    Modify specs and factories with i18n

--- spec/models/by_search_word_tweet_spec.rb
+++ spec/models/by_search_word_tweet_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe BySearchWordTweet, type: :model do
+  let(:by_search_word_tweet) do
+    build(:tweet, :by_search_word)
+  end
+
   describe "'search_word' column" do
     it 'If not exist, a validation error occurs' do
-      by_search_word_tweet = BySearchWordTweet.new(
-        id_number: 54321,
-        full_text: 'Hello, World!',
-        serialized_object: {}
-      )
-      by_search_word_tweet.user = FactoryBot.build(:user)
+      by_search_word_tweet.search_word = nil
 
       by_search_word_tweet.valid?
       expect(by_search_word_tweet.errors.messages).not_to be_empty
+    end
+
+    it 'If exists, valid' do
+      by_search_word_tweet.valid?
+      expect(by_search_word_tweet.errors.messages).to be_empty
     end
   end
 end
